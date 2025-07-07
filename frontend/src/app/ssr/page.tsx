277d65e4a74a85bc30a316092e3812af70131b1e
JuHyang <ljh28891004@gmail.com>
2025-07-07 22:04:56 +0900

    init project

---
+++ frontend/src/app/ssr/page.tsx
@@ -0,0 +1,18 @@
+export default async function Home() {
+  const message = await getData();
+
+  return (
+    <div>
+      <h1>SSR with NestJS</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+async function getData() {
+  const res = await fetch('http://localhost:3001/hello', {
+    // SSR 캐싱 무효화
+    cache: 'no-store',
+  });
+  return res.text();
+}
