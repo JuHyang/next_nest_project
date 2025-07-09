9bead3cf4c6d1c14a483c670b8bf5ffd69f8451a
JuHyang <ljh28891004@gmail.com>
2025-07-09 17:13:47 +0900

    add cart pages

---
+++ frontend/store/src/app/cart/page.tsx
@@ -0,0 +1,11 @@
+import CartView from "@/features/cart/components/CartView";
+
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Cart Page</h1>
+      <CartView />
+    </div>
+  );
+}
